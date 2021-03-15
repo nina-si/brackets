53ea9a8fff0e2e4f139c563f3b7382f58f2630cf
Nina Sitaeva <sitaeva.nn@gmail.com>
2021-03-15 17:22:24 +0300

    init: start task js brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = bracketsConfig.map(function(bracket){
+    return bracket[0]+bracket[1];
+  });
+
+  for (let i=0; i<str.length; i++) {
+    if (!stack.length) stack.push(str[i]);
+    else {
+      if (brackets.includes(stack[stack.length-1] + str[i])) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    }
+  }
+
+  if (!stack.length) return true;
+  else return false;
 }
